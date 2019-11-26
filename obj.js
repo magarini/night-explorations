357276258f48c65839e14d15ac6d2ca290e89431
magarini <56253774+magarini@users.noreply.github.com>
2019-11-26 15:43:32 +0200

    Update obj.js

--- obj.js
+++ obj.js
@@ -18,24 +18,19 @@ function setup() {
 function draw() {
   background(0);
   angleMode(DEGREES);
-//   scale(10); // Scaled to make model fit into canvas
 push();
   rotateX(-90);
   rotateY(frameCount * 0.1);
   
   rotateZ(frameCount * 0.1);
-//   normalMaterial(); // For effect
 scale(20)
 
-//   rotateY(0.5);
 translate(0,0,0)
 texture(img);
 
   model(teapot);
   pop();
-//   directionalLight(51, 102, 126,mouseX, mouseY, 0);
-//   directionalLight(51, 102, 126,mouseX, mouseY, 10);
-//   directionalLight(51, 102, 126,mouseX, mouseY, 10);
+
 
   ambientLight(51, 102, 255);
 
@@ -47,7 +42,7 @@ texture(img);
     fill(255);
     stroke(255);
     push();
-  translate(mouseX-800, mouseY-500) ;
+  translate(mouseX-width/2,mouseY-height/2) ;
   sphere(3);
   pop();
 
@@ -55,7 +50,7 @@ texture(img);
 
 function mousePressed(){
     let r=random(1,4);
-    let b=new Bubble(mouseX-700,mouseY-400,r);
+    let b=new Bubble(mouseX-width/2,mouseY-height/2,r);
     
     bubbles.push(b);
 }
@@ -84,46 +79,7 @@ class Bubble{
   pop();
     }
 
-    // clicked(px,py){
-    //     let d=dist(px,py,this.x,this.y);
-    //     if(d<this.r){
-    //     console.log("clicked");
-    //     this.brightness=255;
-    //     }
+   
     }
 
 
-// }
-
-
-// function keyPressed(){  
-//     let a = random(600);
-//     let b = random(600);
-//    let c = random(600);
-//      let d = random(600);
-//      let e = random(600);
-     
-
-
-//     if(keyCode==SHIFT){
-//    // for (int c = 0; c < i; cc++) {
-//       console.log(`shift`);
-
-//       stroke(255);
-//       strokeWeight(10);
-//       //line(rr, ii*200, 50*rr, ii*rr);
-      
-//       line(a, b, c, d,b, e);
-//       //curve(rr, a, rr, ii*rr,ii+300,399,ii-200,rr*12);
-//      // }
-//     }
-//   }
-// //   else {
-// //     //for (int i = 0; i < 5; i++) {
-// //       //for (int c = 0; c < 10; c++) {
-      
-// //       stroke(255);
-// //       strokeWeight(10);
-// //       line(a, b, c, b);
-      
-// //     }
